64969bb1270eb4d8a062f9982e71bd9f5ec49cf5
Shahin Nezam <shahinnezam@hotmail.com>
2022-05-09 21:12:13 -0700

    Initial map build on map page w/ center coordinates on seattle

--- PollinatorPathway/Live Map VC.swift
+++ PollinatorPathway/Live Map VC.swift
@@ -6,16 +6,32 @@
 //
 
 import UIKit
+import MapKit
 
 class Live_Map_VC: UIViewController {
 
+    @IBOutlet private var mapView: MKMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let initialLocation = CLLocation(latitude: 47.6062, longitude: 122.3321)
+        //mapView.setCenter(initialLocation, animated: false)
         // Do any additional setup after loading the view.
     }
     
-
+    private extension MKMapView {
+      func centerToLocation(
+        _ location: CLLocation,
+        regionRadius: CLLocationDistance = 1000
+      ) {
+        let coordinateRegion = MKCoordinateRegion(
+          center: location.coordinate,
+          latitudinalMeters: regionRadius,
+          longitudinalMeters: regionRadius)
+        setRegion(coordinateRegion, animated: true)
+      }
+    }
     /*
     // MARK: - Navigation
 
