c5c82db9cfdd703d84efed45d6a22641627a371c
Andrea Harrison <Andrealharrison@gmail.com>
2022-09-29 14:00:21 -0700

    created Planting VC, added buttons and segue

---
+++ PollinatorPathway/Planting VC.swift
@@ -0,0 +1,29 @@
+//
+//  Planting VC.swift
+//  PollinatorPathway
+//
+//  Created by Andrea Harrison on 9/29/22.
+//
+
+import UIKit
+
+class Planting_VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
