a26b7df6626198243ac3aa04de577f0656122e65
Shahin Nezam <shahinnezam@hotmail.com>
2022-10-25 14:20:41 -0700

    Debugging but no build errors! button does not create new camera window

--- PollinatorPathway/New User Setup VC.swift
+++ PollinatorPathway/New User Setup VC.swift
@@ -9,24 +9,39 @@ import UIKit
 
 class New_User_Setup_VC: UIViewController {
     
-    @IBOutlet weak var scanBarButton: UIButton!
-    @IBOutlet weak var scanTextField: UITextField!
+    @IBOutlet weak var scanQRbutton: UIButton!
+    @IBOutlet weak var scanQRtext: UITextField!
     let scannerVC = Scanner_VC()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         scannerVC.scannerDelegate = self
     }
+}
     
-    @objc func scanBarTapped() {
-        self.navigationController?.pushViewController(scannerVC, animated: true)
-    }
+    extension New_User_Setup_VC {
+        private func updateUI() {
+            //scan now qr code button
+            scanQRbutton.setTitle("Scan QR Code", for: .normal)
+            scanQRbutton.setTitleColor(UIColor.white, for: .normal)
+            scanQRbutton.addTarget(self, action: #selector(scanQRaction), for: .touchUpInside)
+            //output text box (TEMPORARY)
+            scanQRtext.text = "test output here"
+            scanQRtext.textAlignment = .center
+            scanQRtext.textColor = UIColor.black
+
+        }
+        //function after button
+        @objc func scanQRaction() {
+            self.navigationController?.pushViewController(scannerVC, animated: true)
+        }
 }
     
     extension New_User_Setup_VC: ScannerVD {
         func didFindScannedText(text: String) {
           //Assigning the delegate to scanTextField
-            scanTextField.text = text
+            scanQRtext.text = text
         }
     }
     
