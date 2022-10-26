f98b9b36495d2451644f048302bded15e7d3fb1e
fabricekalvanda <fabricekalvanda@gmail.com>
2022-10-26 15:22:53 -0700

    Set new view controller to handle Login, sign up and Welcome page

---
+++ PollinatorPathway/ViewControllers/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  PollinatorPathway
+//
+//  Created by Student Account on 10/26/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
