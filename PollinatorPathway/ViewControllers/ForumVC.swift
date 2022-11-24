52dc5070abce5fed5a41c617f48f78231623e52d
asb43 <pokemonfanboy600@gmail.com>
2022-11-23 16:26:07 -0800

    Created Forum VC and added The base code

---
+++ PollinatorPathway/ViewControllers/ForumVC.swift
@@ -0,0 +1,19 @@
+//
+//  ForumVC.swift
+//  PollinatorPathway
+//
+//  Created by Student Account on 11/23/22.
+//
+
+import UIKit
+import FirebaseAuth
+
+class Forum_VC: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
