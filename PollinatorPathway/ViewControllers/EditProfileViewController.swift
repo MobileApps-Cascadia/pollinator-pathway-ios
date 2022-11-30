3430eeffb4e56d994d4c87e4b1e3c1832e6b40bb
fabricekalvanda <fabricekalvanda@gmail.com>
2022-11-30 14:29:24 -0800

    setUp UI to add more information

---
+++ PollinatorPathway/ViewControllers/EditProfileViewController.swift
@@ -0,0 +1,71 @@
+//
+//  EditProfileViewController.swift
+//  PollinatorPathway
+//
+//  Created by Student Account on 11/28/22.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+    
+    
+    @IBOutlet weak var websiteTextField: UITextField!
+    
+    
+    @IBOutlet weak var AddEmailTextField: UITextField!
+    
+    
+    @IBOutlet weak var PhoneTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var FacebookTextField: UITextField!
+    
+    
+    @IBOutlet weak var InstagramTextField: UITextField!
+    
+    
+    @IBOutlet weak var AddressTextField: UITextField!
+    
+    
+    @IBOutlet weak var CityTextField: UITextField!
+    
+    
+    @IBOutlet weak var StateTextField: UITextField!
+    
+    
+    @IBOutlet weak var ZipCodeTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var GpsTextField: UITextField!
+    
+    
+    @IBOutlet weak var SubmitButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func SubmitButtonTapped(_ sender: Any) {
+        
+    }
+    
+
+}
